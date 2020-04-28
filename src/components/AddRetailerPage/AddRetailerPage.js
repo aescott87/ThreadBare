3f0a7ec9c88be49b93b018487b8a691f059d37e5
Ashley Scott <aescott87@gmail.com>
2020-04-28 13:21:19 -0500

    Set up basic page structure for Add a Retailer page

--- src/components/AddRetailerPage/AddRetailerPage.js
+++ src/components/AddRetailerPage/AddRetailerPage.js
@@ -1,12 +1,33 @@
 import React, {Component} from 'react';
+//import MUI CSS components
+import Form from 'muicss/lib/react/form';
+import Input from 'muicss/lib/react/input';
+import Checkbox from 'muicss/lib/react/checkbox';
+import Button from 'muicss/lib/react/button';
 
 class AddRetailerPage extends Component {
     render() {
         return(
             <>
             <h2>Add a Retailer</h2>
-            
-            
+            <Form>
+                <legend>Name:</legend>
+                <Input />
+                <legend>Website:</legend>
+                <Input defaultValue="https://" />
+                <legend>Sizes They Carry:</legend>
+                <Checkbox label="Plus Sizes(12-32)" />
+                <Checkbox label="Petite Sizes(00-0)" />
+                <Checkbox label="X-Short Inseam(28 and shorter)" />
+                <Checkbox label="Short Inseam(28-30)" />
+                <Checkbox label="Long Inseam(34-36)" />
+                <Checkbox label="X-Long Inseam(36 and longer)" />
+                <legend>Where You Can Shop:</legend>
+                <Checkbox label="In Store" />
+                <Checkbox label="In Select Stores" />
+                <Checkbox label="Online" />
+                <Button type="submit" color="accent">Add</Button>
+            </Form>
             </>
         )
     }
