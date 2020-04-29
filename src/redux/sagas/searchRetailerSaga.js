4b9eba6f76a7c82f052d770777e515a34ed996cf
Ashley Scott <aescott87@gmail.com>
2020-04-29 15:46:50 -0500

    Set up search retailer Saga

---
+++ src/redux/sagas/searchRetailerSaga.js
@@ -0,0 +1,20 @@
+import { put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
+function* searchRetailer( action ) {
+    console.log('in searchRetailer saga', action.payload);
+    try { 
+        const response = yield axios.get(`/api/retailer`);
+        console.log('Response coming from server', response)
+        yield put({type: 'LIST_RESULT', payload: response.data })
+    }
+    catch(error){
+        console.log('error in search retailer', error);
+    }
+}
+
+function* searchRetailerSaga() {
+    yield takeLatest('SEARCH_RETAILER', searchRetailer);
+  }
+
+export default searchRetailerSaga;
