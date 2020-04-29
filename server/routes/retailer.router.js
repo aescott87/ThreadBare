a23af112cec4821a46b37453e8f10f16b5064773
Ashley Scott <aescott87@gmail.com>
2020-04-29 18:43:16 -0500

    Fixed errors in retailer.router GET request

--- server/routes/retailer.router.js
+++ server/routes/retailer.router.js
@@ -7,14 +7,17 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
  * GET route template
  */
 router.get('/', (req, res) => {
-    console.log('search request', req.query);
-    /*const queryText = 'SELECT title, description FROM movies WHERE id = $1';
-    pool.query(queryText, [req.params.id])
-      .then((result) => { res.send(result.rows[0]); })
+    console.log('search request', req.query.retailerName);
+    const queryText = `SELECT "name", "website", "available", "type" FROM "retailer"
+    JOIN "retailer_size" ON "retailer"."id" = "retailer_size"."retailer_id"
+    JOIN "size" ON "retailer_size"."size_id" = "size"."id"
+    WHERE "name" LIKE ($1);`;
+    pool.query(queryText, [req.query.retailerName])
+      .then((result) => { res.send(result.rows); })
       .catch((error) => {
-        console.log('Error completing SELECT details query', error);
+        console.log('Error completing GET retailer query', error);
         res.sendStatus(500);
-    });*/
+    });
 });
 
 /**
--- src/redux/sagas/searchRetailerSaga.js
+++ src/redux/sagas/searchRetailerSaga.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 function* searchRetailer( action ) {
     console.log('in searchRetailer saga', action.payload);
     try { 
-        const response = yield axios.get(`/api/retailer`);
-        console.log('Response coming from server', response)
+        const response = yield axios.get(`/api/retailer`, { params: { retailerName: action.payload } });
+        console.log('Response coming from server', response.data)
         yield put({type: 'LIST_RESULT', payload: response.data })
     }
     catch(error){
